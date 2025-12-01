01c158df62316625e81b2ec8f1849d99dea034a3
SEM01 <195654711+SEM01@users.noreply.github.com>
2025-12-01 15:49:51 +0330

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,80 @@
+from dataclasses import dataclass, field
+from typing import Any
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(name)s - %(asctime)s - %(levelname)s - %(message)s",
+    filename="reminder.log",
+    filemode="a",
+)
+logger = logging.getLogger()
+
+
+def id_gen():
+    i = 1
+    while True:
+        yield i
+        i += 1
+
+
+id = id_gen()
+
+
+@dataclass
+class Reminder:
+    title: str
+    time: str = "00:00"
+    reminder_id: int = field(default_factory=lambda: next(id))
+
+
+@dataclass
+class SimpleReminder(Reminder):
+    def __post_init__(self):
+        if self.title == "" or self.time == "":
+            logger.error("Empty")
+        else:
+            reminder_list = []
+            reminder_list.append(self.reminder_id)
+            reminder_list.append(self.title)
+            reminder_list.append(self.time)
+            print(reminder_list)
+
+    def __repr__(self):
+        return f"<Task ID:{self.reminder_id}> It is Time: {self.title}"
+
+
+@dataclass
+class MeetingReminder(Reminder):
+    participants: list = Any
+
+    def __post_init__(self):
+        if self.title == "" or self.time == "":
+            logger.error("Empty")
+        else:
+            reminder_list = []
+            reminder_list.append(self.reminder_id)
+            reminder_list.append(self.title)
+            reminder_list.append(self.time)
+            print(reminder_list)
+
+    def __repr__(self):
+        return f"<Task ID:{self.reminder_id}> Meeting Reminder: {self.title}--->{self.participants}"
+
+
+@dataclass
+class DailyRoutineReminder(Reminder):
+    repeat: bool = False
+
+
+@dataclass
+class ReminderManager:
+    def add_reminder():
+        logger.info("Reminder add")
+
+
+task1 = MeetingReminder(title="Speaking", time="10", participants=["a", "b"])
+ReminderManager.add_reminder()
+
+
+task2 = SimpleReminder(title="", time="10")
